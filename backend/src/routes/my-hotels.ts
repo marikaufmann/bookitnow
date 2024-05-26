c45287d7c75b838dee375d2cde88a5988a47ef8d
mariakaufmann <mari.kaufmann20@gmail.com>
2024-05-26 16:27:41 +0300

    refactor(cloudinary): switch to secure URLs

--- backend/src/routes/my-hotels.ts
+++ backend/src/routes/my-hotels.ts
@@ -114,7 +114,7 @@ const uploadImages = async (imageFiles: Express.Multer.File[]) => {
 		const b64 = Buffer.from(image.buffer).toString('base64')
 		let dataURI = 'data:' + image.mimetype + ';base64,' + b64
 		const res = await cloudinary.v2.uploader.upload(dataURI)
-		return res.url
+		return res.secure_url
 	})
 	const imageUrls = await Promise.all(uploadPromises)
 	return imageUrls
